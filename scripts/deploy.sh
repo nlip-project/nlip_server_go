857e123dc45597f94c5d24d03794932065b05882
hbzengin <hbzengin@druid.eecs.umich.edu>
2024-10-29 17:35:43 -0400

    Add code signing to prevent pop-up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,13 @@
 # Variables
 EXECUTABLE="/usr/local/bin/nlip"
 PLIST_PATH="/Library/LaunchDaemons/com.nlip.plist"
+BUILD_PATH="./nlip"
+CERT_NAME="NLIPSigningCert"
+KEYCHAIN_PASSWORD=$(cat scripts/.keychain_password)
 
 # Build the Go project
 echo "Building the Go project..."
-sudo go build -o $EXECUTABLE
+go build -o $BUILD_PATH
 
 if [ $? -ne 0 ]; then
   echo "Build failed. Exiting."
@@ -14,12 +17,35 @@ if [ $? -ne 0 ]; then
 fi
 echo "Build succeeded."
 
-# Set permissions for the executable
-echo "Setting permissions for the executable..."
-sudo chmod +x $EXECUTABLE
-sudo chown root:wheel $EXECUTABLE
+# Unlock the keychain
+echo "Unlocking the keychain..."
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" ~/Library/Keychains/login.keychain-db
 
-# This might be needed to process changes correctly
+if [ $? -ne 0 ]; then
+  echo "Failed to unlock keychain. Exiting."
+  exit 1
+fi
+
+# Sign the executable
+echo "Signing the executable..."
+codesign -s "$CERT_NAME" ./nlip
+
+if [ $? -ne 0 ]; then
+  echo "Code signing failed. Exiting."
+  exit 1
+fi
+echo "Code signing succeeded."
+
+# Move the executable to the install path
+echo "Moving the executable to $EXECUTABLE..."
+sudo mv $BUILD_PATH $EXECUTABLE
+
+# Set permissions for the executable
+# echo "Setting permissions for the executable..."
+# sudo chmod +x $EXECUTABLE
+# sudo chown root:wheel $EXECUTABLE
+
+# This seems necessary to process changes correctly
 sleep 0.5
 
 # Reload the launchd service
